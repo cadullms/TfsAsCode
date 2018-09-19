46805e2dabd1223d2aeed9c41d72a461a605a37a
Carsten Düllmann <cadull@microsoct.com>
2018-09-19 15:55:57 +0200

    first shot at something runnable...

---
+++ Artifacts/TfsInstall/artifact.ps1
@@ -0,0 +1,103 @@
+Param(
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("2015")] 
+    [string] $version
+)
+
+function DownloadToFilePath ($downloadUrl, $targetFile)
+{
+    Write-Output ("Downloading installation files from URL: $downloadUrl to $targetFile")
+    $targetFolder = Split-Path $targetFile
+
+    if((Test-Path -path $targetFile))
+    {
+        Write-Output "Deleting old target file $targetFile"
+        Remove-Item $targetFile -Force | Out-Null
+    }
+
+    if(-not (Test-Path -path $targetFolder))
+    {
+        Write-Output "Creating folder $targetFolder"
+        New-Item -ItemType Directory -Force -Path $targetFolder | Out-Null
+    }
+
+    #Download the file
+    $downloadAttempts = 0
+    do
+    {
+        $downloadAttempts++
+
+        try
+        {
+            $WebClient = New-Object System.Net.WebClient
+            $WebClient.DownloadFile($downloadUrl,$targetFile)
+            break
+        }
+        catch
+        {
+            Write-Output "Caught exception during download..."
+            if ($_.Exception.InnerException){
+                Write-Output "InnerException: $($_.InnerException.Message)"
+            }
+            else {
+                Write-Output "Exception: $($_.Exception.Message)"
+            }
+        }
+
+    } while ($downloadAttempts -lt 5)
+
+    if($downloadAttempts -eq 5)
+    {
+        Write-Error "Download of $downloadUrl failed repeatedly. Giving up."
+    }
+}
+
+# ============ this is our entry point ==================
+
+Write-Output "Installing Team Foundation Server $version"
+$logFolder = Join-path -path $env:ProgramData -childPath "DTLArt_TFS"
+
+if($version -eq '2015' )
+{
+    $tfsInstallLog = Join-Path $logFolder "TFSInstall.log"
+    $argumentList = "/Quiet /Log $tfsInstallLog"
+    $downloadUrl = 'https://download.my.visualstudio.com/db/en_team_foundation_server_express_2015_update_4_x86_x64_web_installer_11701809.exe?t=04011165-51a9-407e-ac68-462d80685ed2&e=1537400301&h=0715ecd3ddbd2c2fdd5162a2fbc8f2f6&su=1'
+}
+else
+{
+    Write-Error "Version is not recognized - allowed value is 2015. Specified value: $version"
+}
+
+$localFile = Join-Path $logFolder 'tfsinstaller.exe'
+DownloadToFilePath $downloadUrl $localFile
+
+Write-Output "Running install with the following arguments: $argumentList"
+$retCode = Start-Process -FilePath $localFile -ArgumentList $argumentList -Wait -PassThru
+
+if ($retCode.ExitCode -ne 0 -and $retCode.ExitCode -ne 3010)
+{
+    if($version -eq '2017')
+    {
+        $targetLogs = 'c:\VS2017Logs'
+        New-Item -ItemType Directory -Force -Path $targetLogs | Out-Null
+        Write-Output ('Temp location is ' + $env:TEMP)
+        Copy-Item -path $env:TEMP\dd* -Destination $targetLogs
+    }
+
+    Write-Error "Product installation of $localFile failed with exit code: $($retCode.ExitCode.ToString())"    
+}
+else
+{
+    Write-Output "Team Foundation Server install succeeded."
+}
+
+Write-Output "Configuring Team Foundation Server $version"
+# https://blogs.msdn.microsoft.com/devops/2012/10/12/unattended-installation-of-team-foundation-server-20122013/
+# https://docs.microsoft.com/en-us/vsts/tfs-server/command-line/tfsconfig-cmd#identities
+
+$TfsToolsDir ="C:\Program Files\Microsoft Team Foundation Server 14.0\Tools"
+& "$TfsToolsDir\tfsconfig.exe" unattend /create /type:STANDARD /unattendfile:$logFolder\standard.ini "/inputs:StartTrial=false;IsServiceAccountBuiltIn=False;UseReporting=False;UseWss=False"
+& "$TfsToolsDir\tfsconfig.exe" unattend /configure /unattendfile:$logFolder\standard.ini /continue
+
+Write-Output "Configured Team Foundation Server $version. I guess. We will need to check the exit code here and stuff, but I need some food first, gonna let this run during lunch break..."
+
